998d221bec7f57bf4cad15ff16cefa15aa7853bf
Itai Caspi <itaicaspi@gmail.com>
2019-07-09 13:35:41 +0300

    firebase face detection

--- app/src/main/java/com/urbanize/urbanizeplayer/MainActivity.kt
+++ app/src/main/java/com/urbanize/urbanizeplayer/MainActivity.kt
@@ -3,12 +3,13 @@ package com.urbanize.urbanizeplayer
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Matrix
+import android.graphics.*
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.util.Rational
 import android.util.Size
 import android.view.Surface
@@ -22,14 +23,140 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.google.firebase.ml.vision.face.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.security.Permissions
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
     private var lensFacing = CameraX.LensFacing.FRONT
     private val TAG = "MainActivity"
 
+    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()
+            val data = ByteArray(remaining())
+            get(data)
+            return data
+        }
+
+        fun ImageProxy.toBitmap(): Bitmap {
+            val yBuffer = planes[0].buffer // Y
+            val uBuffer = planes[1].buffer // U
+            val vBuffer = planes[2].buffer // V
+
+            val ySize = yBuffer.remaining()
+            val uSize = uBuffer.remaining()
+            val vSize = vBuffer.remaining()
+
+            val nv21 = ByteArray(ySize + uSize + vSize)
+
+            //U and V are swapped
+            yBuffer.get(nv21, 0, ySize)
+            vBuffer.get(nv21, ySize, vSize)
+            uBuffer.get(nv21, ySize + vSize, uSize)
+
+            val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
+            val out = ByteArrayOutputStream()
+            yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
+            val imageBytes = out.toByteArray()
+
+//            return imageBytes
+            return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+        }
+
+
+        private val detector: FirebaseVisionFaceDetector by lazy {
+            FirebaseVision.getInstance().getVisionFaceDetector()
+        }
+
+        var pendingTask: Task<out Any>? = null
+
+        override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+//            val currentTimestamp = System.currentTimeMillis()
+            // Throttle calls to the detector.
+            if (pendingTask != null && !pendingTask!!.isComplete) {
+                Log.d("MLQRcodeAnalyzer", "Throttle calls to the detector")
+                return
+            }
+            //YUV_420 is normally the input type here
+            var rotation = rotationDegrees % 360
+            if (rotation < 0) {
+                rotation += 360
+            }
+            val mediaImage = FirebaseVisionImage.fromMediaImage(image.image!!, when (rotation) {
+                0 -> FirebaseVisionImageMetadata.ROTATION_0
+                90 -> FirebaseVisionImageMetadata.ROTATION_90
+                180 -> FirebaseVisionImageMetadata.ROTATION_180
+                270 -> FirebaseVisionImageMetadata.ROTATION_270
+                else -> {
+                    Log.e("MLQRcodeAnalyzer", "unexpected rotation: $rotationDegrees")
+                    FirebaseVisionImageMetadata.ROTATION_0
+                }
+            })
+
+            pendingTask = detector.detectInImage(mediaImage)
+                .addOnSuccessListener { faces ->
+                    // Task completed successfully
+                    for (face in faces) {
+                        val bounds = face.boundingBox
+                        val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
+                        val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
+
+                        // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                        // nose available):
+//                        val leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR)
+//                        leftEar?.let {
+//                            val leftEarPos = leftEar.position
+//                        }
+//
+//                        // If contour detection was enabled:
+//                        val leftEyeContour = face.getContour(FirebaseVisionFaceContour.LEFT_EYE).points
+//                        val upperLipBottomContour = face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).points
+//
+//                        // If classification was enabled:
+//                        if (face.smilingProbability != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+//                            val smileProb = face.smilingProbability
+//                        }
+//                        if (face.rightEyeOpenProbability != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+//                            val rightEyeOpenProb = face.rightEyeOpenProbability
+//                        }
+//
+//                        // If face tracking was enabled:
+//                        if (face.trackingId != FirebaseVisionFace.INVALID_ID) {
+//                            val id = face.trackingId
+//                        }
+                        Log.d("firebaseVision", bounds.toString())
+                    }
+                }
+                .addOnFailureListener(
+                    object : OnFailureListener {
+                        override fun onFailure(e: Exception) {
+                            // Task failed with an exception
+                            // ...
+                        }
+                    })
+
+//            if (currentTimestamp - lastAnalyzedTimestamp >=
+//                TimeUnit.SECONDS.toMillis(1)) {
+//                val buffer = image.planes[0].buffer
+//                val data = buffer.toByteArray()
+//                val pixels = data.map { it.toInt() and 0xFF }
+//                val luma = pixels.average()
+//                Log.d("CameraXApp", "Average luminosity: $luma")
+//                lastAnalyzedTimestamp = currentTimestamp
+//            }
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +176,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // camera handling
-        textureView.post { startCamera() }
+//        textureView.post { startCamera() }
 
         val cameraAvailable = checkCameraHardware(this)
         if (!cameraAvailable) {
@@ -61,7 +188,11 @@ class MainActivity : AppCompatActivity() {
         mainWebView.webChromeClient = WebChromeClient()
         mainWebView.settings.javaScriptEnabled = true
         mainWebView.settings.mediaPlaybackRequiresUserGesture = false
-        mainWebView.loadUrl("file:///android_asset/video.html")
+        mainWebView.settings.setAppCacheEnabled(true)
+        mainWebView.settings.domStorageEnabled = true
+        mainWebView.settings.databaseEnabled = true
+        mainWebView.loadUrl("http://10.42.0.1:5000/dynamic_content")
+//        mainWebView.loadUrl("file:///android_asset/video.html")
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -84,19 +215,24 @@ class MainActivity : AppCompatActivity() {
             updateTransform()
         }
 
-        // Create configuration object for the image capture use case
-        val imageCaptureConfig = ImageCaptureConfig.Builder()
+        val imageAnalysisConfig = ImageAnalysisConfig.Builder()
             .apply {
                 setLensFacing(lensFacing)
                 setTargetAspectRatio(screenAspectRatio)
                 setTargetRotation(textureView.display.rotation)
-                setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
-            }.build()
+//                setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+//                setImageQueueDepth(1)
+            }
+            .build()
+        val imageAnalysis = ImageAnalysis(imageAnalysisConfig).apply {
+            analyzer = LuminosityAnalyzer()
+        }
 
-        // Build the image capture use case and attach button click listener
-        val imageCapture = ImageCapture(imageCaptureConfig)
+//        imageAnalysis.setAnalyzer({ image: ImageProxy, rotationDegrees: Int ->
+//            // insert your code here.
+//        })
 
-        CameraX.bindToLifecycle(this, preview, imageCapture)
+        CameraX.bindToLifecycle(this, preview, imageAnalysis)
     }
 
     private fun updateTransform() {
